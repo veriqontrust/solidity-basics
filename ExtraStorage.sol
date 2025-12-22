f794223a401c40224149f44fb4978e61b8f14171
veriqontrust <veriqon@gmail.com>
2025-12-22 18:53:35 +0530

    Update ExtraStorage.sol

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 
 import "./SimpleStorage.sol";
 
-contract ExtraStorage is SimpleStorage {//this is inheritance , we made this contract inherit all functions from that contract by making this the child contract.
-   /*if we were to modify any function we don't like from the parent contract,
+contract ExtraStorage is SimpleStorage {
+   /*
+   this is inheritance , we made this contract inherit all functions from that contract by making this the child contract.
+   If we were to modify any function we don't like from the parent contract,
    then we can do it using these keywords #virtual ,override
    
    we have to add virtual to the parent function wewhat to override here , and add override to the child contract , to override the data.
